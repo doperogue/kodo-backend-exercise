10fb50eb0c65e3b16aec9f44ea59aa33026d14ad
doperogue <yogeshwar.maithani@gmail.com>
2022-10-02 19:43:43 +0530

    feature: mock data repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MockDataRepoService } from './mock-data-repo/mock-data-repo.service';
+
+const mockDataRepoProvider = {
+  provide: 'MockDataRepository',
+  useValue: null,
+};
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MockDataRepoService, mockDataRepoProvider],
 })
 export class AppModule {}
