ead6ed5cbcebb80c71b5a77c425070e5481f6ede
doperogue <yogeshwar.maithani@gmail.com>
2022-10-02 09:45:08 +0530

    feat: basic searching

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ListDto } from './dtos/List.dto';
+import { MockData } from './data.store';
+import { MockDataInterface } from './interfaces/mockData.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public findAll(): MockDataInterface[] {
+    return MockData;
+  }
+
+  public list(listDto: ListDto): MockDataInterface[] {
+    return MockData.filter((element: MockDataInterface) => {
+      return (
+        element.name.includes(listDto.searchString) ||
+        element.description.includes(listDto.searchString)
+      );
+    });
   }
 }
---
+++ src/interfaces/mockData.interface.ts
@@ -0,0 +1,6 @@
+export interface MockDataInterface {
+  name: string;
+  image: string;
+  description: string;
+  dateLastEdited: string | Date;
+}
